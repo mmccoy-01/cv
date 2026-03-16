61b9a012d0883e966420be77784efcd5fc2b0eb5
Michael McCoy <91561019+mmccoy-01@users.noreply.github.com>
2026-03-16 14:36:51 -0400

    Fix github workflow action

--- functions.R
+++ functions.R
@@ -1,32 +1,25 @@
-library(pander)
-library(stringr)
-library(dplyr)
-library(googlesheets4)
-library(lubridate)
-library(kableExtra)
-
-gs4_deauth()
+googlesheets4::gs4_deauth()
 
 gscholar_stats <- function(url) {
   cites <- get_cites(url)
-  return(paste(
-    'Citations:', cites$citations, '•',
-    'h-index:',   cites$hindex, '•',
-    'i10-index:', cites$i10index
-  ))
+  paste(
+    "Citations:", cites$citations, "•",
+    "h-index:", cites$hindex, "•",
+    "i10-index:", cites$i10index
+  )
 }
 
 get_cites <- function(url) {
   html <- xml2::read_html(url)
-  node <- rvest::html_nodes(html, xpath='//*[@id="gsc_rsb_st"]')
+  node <- rvest::html_nodes(html, xpath = '//*[@id="gsc_rsb_st"]')
   cites_df <- rvest::html_table(node)[[1]]
-  cites <- data.frame(t(as.data.frame(cites_df)[,2]))
-  names(cites) <- c('citations', 'hindex', 'i10index')
-  return(cites)
+  cites <- data.frame(t(as.data.frame(cites_df)[, 2]))
+  names(cites) <- c("citations", "hindex", "i10index")
+  cites
 }
 
 get_cv_sheet <- function(sheet, audience = params$audience) {
-  df <- read_sheet(
+  df <- googlesheets4::read_sheet(
     ss = "https://docs.google.com/spreadsheets/d/1GmjuFlFnyhJNCXqtyO6mrwYf84ptDxyKy4O6O_A7zKY/edit?usp=sharing",
     sheet = sheet
   )
@@ -49,36 +42,35 @@ get_cv_sheet <- function(sheet, audience = params$audience) {
 }
 
 make_ordered_list <- function(x) {
-    return(pandoc.list(x, style = 'ordered'))
+  pander::pandoc.list(x, style = "ordered")
 }
 
 make_bullet_list <- function(x) {
-  return(pandoc.list(x, style = 'bullet'))
+  pander::pandoc.list(x, style = "bullet")
 }
 
 make_ordered_list_filtered <- function(df, cat) {
-  return(df %>%
-    filter(category == {{cat}}) %>%
-        mutate(
-            citation = str_replace_all(
-                citation,
-                "\\\\\\*(\\w+),",
-                "\\\\*\\\\underline{\\1},"
-            )
-        ) %>%
-    pull(citation) %>%
+  df %>%
+    dplyr::filter(category == {{ cat }}) %>%
+    dplyr::mutate(
+      citation = stringr::str_replace_all(
+        citation,
+        "\\\\\\*(\\w+),",
+        "\\\\*\\\\underline{\\1},"
+      )
+    ) %>%
+    dplyr::pull(citation) %>%
     make_ordered_list()
-  )
 }
 
 na_to_space <- function(x) {
-  return(ifelse(is.na(x), '', x))
+  ifelse(is.na(x), "", x)
 }
 
 enquote <- function(x) {
-  return(paste0('"', x, '"'))
+  paste0('"', x, '"')
 }
 
 markdown_url <- function(url) {
-  return(paste0('[', url, '](', url,')'))
+  paste0("[", url, "](", url, ")")
 }
